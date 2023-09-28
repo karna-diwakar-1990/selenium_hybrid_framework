b75a29001f79122057565228add67c96fc137a97
Karna Diwakar <diwakarkarna@live.com>
2023-09-28 15:02:24 +0530

    Rectified the mistake of not adding resources folder

--- src/test/java/com/karna/sample_Test.java
+++ src/test/java/com/karna/sample_Test.java
@@ -3,6 +3,8 @@ package com.karna;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.karna.Utils.Logger.Log;
+
 public class sample_Test {
 
     @Test
@@ -10,13 +12,16 @@ public class sample_Test {
         int a = 10;
         int b = 10;
         Assert.assertEquals(a+b, 20);
+        Log.info("Completed Test");
     }
 
     @Test
     public void subtract(){
-        int a = 20;
-        int b = 20;
-        Assert.assertEquals(a-b, 0);
+       
+            int a = 20;
+            int b = 20;
+            Assert.assertEquals(a-b, 1);
+            Log.info("Completed Test");    
     }
     
 }
